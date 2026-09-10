8cbe96bd0010dc9b21e4531efebef37d703397f5
Dmitriy Potykalov <dmitriy.potykalov@gmail.com>
2026-09-10 23:39:47 +0300

    initial commit

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,22 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import webpack from "webpack";
+import type { Configuration } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import type {} from "webpack-dev-server";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const config: webpack.Configuration = {
+const config: Configuration = {
   entry: "./src/index.ts",
+
+  devServer: {
+    port: 8080,
+    open: true,
+    hot: true,
+    watchFiles: ["src/**/*.html"],
+  },
 
   module: {
     rules: [
@@ -15,11 +25,27 @@ const config: webpack.Configuration = {
         exclude: /node_modules/,
         use: "babel-loader",
       },
+      {
+        test: /\.html$/i,
+        use: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif|webp|avif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".ts", ".js"],
   },
 
   output: {
@@ -27,6 +53,15 @@ const config: webpack.Configuration = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
 };
 
 export default config;
